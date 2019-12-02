07ffabf9a407682249afc16ddfb7bcee18e660e2
Evgeny Gulyaev <wardercompany@gmail.com>
2019-12-02 14:58:58 +0400

    Add buttons support

--- index.js
+++ index.js
@@ -4,17 +4,33 @@ const { url, messageError: message, choicesMsg, todayWeek } = require('./constan
 class Parser {
   constructor() {
     this.url = url;
+    this.keyboard = {};
   }
 
-  parseChoices(choices){
-    return `${choicesMsg}\n` + choices.reduce((messageRes, { name }) => messageRes + name + '\n ', '');
+  parseChoices(choices) {
+    const buttons = choices.map(({ name }) => ({
+      action: {
+        type: 'text',
+        payload: {
+          button: name,
+        },
+        label: name
+      },
+      color: 'primary',
+    }));
+    this.keyboard = {
+      one_time: false,
+      buttons
+    };
+    return `${choicesMsg}\n`;
   }
 
-  parseDataTable({week, table}){
+  parseDataTable({ week, table }) {
+    this.keyboard = {};
     let res = `${todayWeek} ${week} неделя \n`;
 
-    for(let dayIndex = 2; dayIndex < table.length; dayIndex++ ) {
-      for(let timeIndex = 0; timeIndex < table[dayIndex].length; timeIndex++ ) {
+    for (let dayIndex = 2; dayIndex < table.length; dayIndex++) {
+      for (let timeIndex = 0; timeIndex < table[dayIndex].length; timeIndex++) {
         const value = table[dayIndex][timeIndex];
         res += !timeIndex ? `День ${value} \n` :
           `${table[0][timeIndex]} ${table[1][timeIndex]} ${value} \n `
@@ -24,12 +40,13 @@ class Parser {
   }
 
   async scrapData(name) {
-    const res = { message };
+    const res = { message, keyboard: {} };
     const asuUrl = `${this.url}${encodeURIComponent(name)}`;
     try {
       const { data = {} } = await axios.get(asuUrl);
       const { choices = [], table = [] } = data;
       res.message = choices.length ? this.parseChoices(choices) : this.parseDataTable(table);
+      res.keyboard = this.keyboard;
       return res;
     }
     catch (e) {
